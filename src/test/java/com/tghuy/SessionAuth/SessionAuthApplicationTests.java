6097bb2fc0a8bb6285dd00601f3ce61d5890373c
TranHuy2k2 <giahuytran2002@gmail.com>
2023-08-10 00:45:52 +0700

    chore: init project

---
+++ src/test/java/com/tghuy/SessionAuth/SessionAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tghuy.SessionAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
